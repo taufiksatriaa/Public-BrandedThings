445d5cce9674ddfeea58d84e980148b5be0313d2
taufiksatriaa <kamikazesatria@gmail.com>
2023-11-13 00:41:36 +0700

    done

--- publicSite/src/Components/Navbar.jsx
+++ publicSite/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Branded things
+          EraSmartPhonelah
         </a>
         <button
           className="navbar-toggler"
@@ -17,18 +17,7 @@ const Navbar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link active" href="#">
-                Product
-              </a>
-            </li>
-          </ul>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
           <form className="d-flex">
             <input
               className="form-control me-2"
